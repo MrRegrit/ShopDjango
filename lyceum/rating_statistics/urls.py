291359f63ed45bd6caf61d46926159bacb5897e5
DRAKUTONT <grudnezlov@gmail.com>
2023-11-21 18:30:01 +0300

    fixed invalid view name

--- lyceum/rating_statistics/urls.py
+++ lyceum/rating_statistics/urls.py
@@ -7,17 +7,17 @@ app_name = "rating_statistics"
 urlpatterns = [
     django.urls.path(
         "user_statistics/<int:pk>/",
-        rating_statistics.views.UserStatistic.as_view(),
+        rating_statistics.views.UserStatistics.as_view(),
         name="user_statistics",
     ),
     django.urls.path(
         "profile_statistics/",
-        rating_statistics.views.ProfileStatistic.as_view(),
+        rating_statistics.views.ProfileStatistics.as_view(),
         name="profile_statistics",
     ),
     django.urls.path(
         "item_statistics/<int:pk>/",
-        rating_statistics.views.ItemStatistic.as_view(),
+        rating_statistics.views.ItemStatistics.as_view(),
         name="item_statistics",
     ),
 ]
