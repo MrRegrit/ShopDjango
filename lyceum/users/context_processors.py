4a7c5b365f4dee87a0f898b656cfe1211ffe2553
Andrew <andrey777-novikov777@yandex.ru>
2023-11-21 17:22:04 +0300

    Added several birthday context preprocessor tests
    + Presense test that checks if context variable is present in every request
    
    + Tests that checks if the db query is valid or not
    
    + Test that checks timezone shift validity

--- lyceum/users/context_processors.py
+++ lyceum/users/context_processors.py
@@ -4,11 +4,16 @@ import django.utils.timezone
 import users.models
 
 
-def birthday_users(request: django.http.HttpRequest):
-    current_day = django.utils.timezone.now().date().day
+def birthday_users(request):
+    current_date = django.utils.timezone.now().date()
     birthday_users = (
-        users.models.User.objects.filter(profile__birthday__day=current_day)
+        users.models.User.objects.select_related("profile")
+        .filter(
+            profile__birthday__day=current_date.day,
+            profile__birthday__month=current_date.month,
+        )
         .order_by("?")
+        .only("username", "email")
         .all()
     )
     return {"today_birthdays": birthday_users}
